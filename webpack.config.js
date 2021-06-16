956db3d5be7637697c4bc37f716c7c63820fe91b
Piero Quelopana <pierqs@gmail.com>
2021-06-16 07:12:17 -0500

    webpack, babel, eslint and react  configured

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = function (env) {
+  const isProd = !!env.production;
+  // const ENVIRONMENT = isProd ? 'production' : 'development';
+
+  const entry = [path.resolve(__dirname) + '/src/index.js'];
+
+  return {
+    mode: isProd?'production':'development',
+    entry: entry,
+    devtool: "source-map",
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    output: {
+      filename: 'app.bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'ts-loader'
+            }
+          ]
+        },
+        {
+          test: /\.js(x?)$/,
+          enforce: 'pre',
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'source-map-loader'
+            },
+            {
+              loader: 'babel-loader'
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: 'src/index.html' })
+    ],
+    // externals: {
+    //   'react': 'React',
+    //   'react-dom': 'ReactDOM'
+    // }
+  };
+}
